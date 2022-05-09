1439e67bd67f1820e0a9351ecc8527d00a6a7d51
ObsidianZed <zechman4@hotmail.com>
2022-05-09 14:32:41 -0500

    Add grid customizer and user prompt

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
 const container = document.querySelector('#container');
-let gridSize = 10;
+let gridSize = 16;
 let gridMapSize = 400;
-let gridSquareSize = (gridMapSize/gridSize) + "px";
-
 
 const gridMap = document.createElement('div');
 gridMap.setAttribute('id','gridMap');
+gridMap.setAttribute('style','height:400px;width:400px;')
 container.appendChild(gridMap);
 
-// build grid by adding gridSize # of divs in a row and create gridSize # of rows
-
-// build one row of gridSize # of divs
+function initializeGrid(){
+    while(gridMap.hasChildNodes()) {
+        gridMap.removeChild(gridMap.firstChild);
+    }
+}
 
 function drawGrid() {
+    let gridSquareSize = (gridMapSize/gridSize) + "px";
     for (i=0; i<gridSize; i++) {
         const gridRow = document.createElement('div');
         gridRow.classList.add('gridRow');
         gridMap.appendChild(gridRow);
-        // drawRows();
         for (k=0; k<gridSize; k++) {
             const gridSquare = document.createElement('div');
             gridSquare.classList.add('gridSquare');
-            // gridSquare.innerHTML += "X";
             gridRow.appendChild(gridSquare);
         }
     }
     const gridSquares = document.getElementsByClassName('gridSquare');
     for (let j=0; j<gridSquares.length; j++) {
-        gridSquares[j].setAttribute("style",`height:${gridSquareSize};width:${gridSquareSize};`);
+        gridSquares[j].setAttribute("style",`height:${gridSquareSize};width:${gridSquareSize};border:1px solid black`);
         gridSquares[j].addEventListener('mouseover', function () {this.style.backgroundColor = "red";});
         gridSquares[j].addEventListener('mouseout', function () {this.style.backgroundColor = "none";});
     }
 }
 
-function drawRows() {
-    const newDiv = document.createElement('div');
-    newDiv.classList.add('gridSquare');
-    // newDiv.innerHTML = "X";
-    gridRow.appendChild(newDiv);
+function userGrid() {
+    gridSize = prompt('How many squares should the grid consist of?');
+    initializeGrid();
+    drawGrid();
 }
 
 drawGrid();
