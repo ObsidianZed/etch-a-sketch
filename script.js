1c18400ffe73becbc3cccaa10971957fc6e97648
ObsidianZed <zechman4@hotmail.com>
2022-05-09 12:43:19 -0500

    Loop make grid layout and add hover fill concept

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('#container');
-let gridSize = 5;
-let gridSquareSize = 10;
+let gridSize = 10;
+let gridMapSize = 400;
+let gridSquareSize = (gridMapSize/gridSize) + "px";
 
 
 const gridMap = document.createElement('div');
@@ -16,25 +17,26 @@ function drawGrid() {
         const gridRow = document.createElement('div');
         gridRow.classList.add('gridRow');
         gridMap.appendChild(gridRow);
-        console.log(`Row ${i}`);
         // drawRows();
-        for (i=0; i<gridSize; i++) {
+        for (k=0; k<gridSize; k++) {
             const gridSquare = document.createElement('div');
             gridSquare.classList.add('gridSquare');
-            gridSquare.innerHTML += "X";
+            // gridSquare.innerHTML += "X";
             gridRow.appendChild(gridSquare);
-            console.log(`Square ${i}`);
-            console.log(`Square Complete.`);
         }
-        console.log(`Row Complete.`);
     }
-    console.log(`Grid Complete.`);
+    const gridSquares = document.getElementsByClassName('gridSquare');
+    for (let j=0; j<gridSquares.length; j++) {
+        gridSquares[j].setAttribute("style",`height:${gridSquareSize};width:${gridSquareSize};`);
+        gridSquares[j].addEventListener('mouseover', function () {this.style.backgroundColor = "red";});
+        gridSquares[j].addEventListener('mouseout', function () {this.style.backgroundColor = "none";});
+    }
 }
 
 function drawRows() {
     const newDiv = document.createElement('div');
     newDiv.classList.add('gridSquare');
-    newDiv.innerHTML = "X";
+    // newDiv.innerHTML = "X";
     gridRow.appendChild(newDiv);
 }
 
